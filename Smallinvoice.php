5d80036b432e5bc9ecb21f9581282bd457a6ffef
pingencom <info@pingen.com>
2013-05-20 10:48:20 +0200

    adding initial class #2

--- Smallinvoice.php
+++ Smallinvoice.php
@@ -1,18 +1,54 @@
 <?php
 
 /**
- * Description of ${NAME}
+ * A class to use the API of smallinvoice.com as an integrator (Version 1.00)
  *
- * @author Łukasz Leśniewski (kontakt@lukaszlesniewski.pl)
- * 25/03/13 11:57
+ * For more information about smallinvoice and how to use it as an integrator see
+ * http://www.smallinvoice.ch/CH/en/home/Create-and-manage-invoices-and-offers-for-free.html
+ *
+ * API documentation can be found here:
+ * http://www.smallinvoice.ch/api/general/overview
+ *
+ *
+ *  Copyright (c) 2013, Lourens Systems GmbH
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the <organization> nor the
+ *  names of its contributors may be used to endorse or promote products
+ *  derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @link http://www.smallinvoice.ch/api/general/overview
  */
 
 class Smallinvoice
 {
     /**
+     * @constant string Library-Version
+     */
+    const VERSION = 1.00;
+
+    /**
      * @var string Base URL of Smallinvoice API
      */
-    protected $sBaseURL = 'https://dev-api.smallinvoice.com';
+    protected $sBaseURL = 'https://api.smallinvoice.com';
 
     /**
      * @var string Auth token
