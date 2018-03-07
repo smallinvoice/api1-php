cfdb2416a4268fb33a9d846883a71268e2a5fda1
smallinvoice <support@smallinvoice.com>
2018-03-07 13:06:55 +0100

    updating example

--- examples/src/InvoiceAdd.php
+++ examples/src/InvoiceAdd.php
@@ -4,72 +4,72 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use smallinvoice\Smallinvoice;
 
-$smallinvoice = new Smallinvoice('mytoken');
+$smallinvoice = new Smallinvoice('INSERTYOURTOKEN');
 
 try {
     /*
      * Creating address object using standard class
      */
-    $objAddress           = new \stdClass();
-    $objAddress->primary  = 1;
-    $objAddress->street   = "Longstreet";
-    $objAddress->streetno = "5";
-    $objAddress->street2  = "";
-    $objAddress->code     = 3122;
-    $objAddress->city     = "Kehrsatz";
-    $objAddress->country  = "CH";
+    $address           = new \stdClass();
+    $address->primary  = 1;
+    $address->street   = "Examplestreet";
+    $address->streetno = "10";
+    $address->street2  = "";
+    $address->code     = 8000;
+    $address->city     = "Zurich";
+    $address->country  = "CH";
 
     /*
      * Creating client object using standard class and attaching collection of addresses' objects
      */
-    $objClient            = new \stdClass();
-    $objClient->type      = 1;
-    $objClient->gender    = 1;
-    $objClient->name      = 'ACME GmbH';
-    $objClient->language  = 'de';
-    $objClient->addresses = array($objAddress);
+    $client            = new \stdClass();
+    $client->type      = 1;
+    $client->gender    = 1;
+    $client->name      = 'ACME GmbH';
+    $client->language  = 'en';
+    $client->addresses = array($address);
 
-    $objClientResponse = $smallinvoice->client_add($objClient);
-    $objClientResponse = $smallinvoice->client_get($objClientResponse->id);
+    $clientAddResponse = $smallinvoice->client_add($client);
+    $clientGetResponse = $smallinvoice->client_get($clientAddResponse->id);
 
     /*
      * Creating position object using array object class
      */
-    $objPosition = new \ArrayObject(array(
-        'type'        => 1,
-        'number'      => 'X-25',
-        'name'        => 'Product X 25',
-        'description' => 'Enjoy sailing your boat with the new X 25',
-        'cost'        => 100.85,
+    $position = new \ArrayObject(array(
+        'type'        => 2,
+        'number'      => 'X25',
+        'name'        => 'Product X25',
+        'description' => 'Enjoy sailing your boat with the new X25 Yaaadrive',
+        'cost'        => 1000.85,
         'unit'        => 7,
         'amount'      => 1,
-        'vat'         => 8,
+        'vat'         => 7.7,
         'discount'    => 0
     ));
 
     /*
      * Creating invoice object using array object class and attaching collection of positions' array objects
      */
-    $objInvoice = new \ArrayObject(array(
+    $invoice = new \ArrayObject(array(
         //'number'            => 'N-34245', //Optional, if you would like to specify the invoice number yourself
-        'client_id'         => $objClientResponse->item->id,
-        //'client_address_id' => $objClientResponse->item->addresses[0]->id, //Optional if you would like a different address
-        //'client_contact_id' => $objClientResponse->item->contacts[0]->id, //Optional if you would like to have a contact
+        'client_id'         => $clientGetResponse->item->id,
+        //'client_address_id' => $clientGetResponse->item->addresses[0]->id, //Optional if you would like a different address
+        //'client_contact_id' => $clientGetResponse->item->contacts[0]->id, //Optional if you would like to have a contact
         'introduction'      => 'Thank you for buying our products',
         'conditions'        => 'Please pay as soon as possible',
         'currency'          => 'CHF',
-        'date'              => date('Y-m-d'),
-        'due'               => date('Y-m-d'),
-        'language'          => 'de',
+        'date'              => '2018-03-01',
+        'due'               => '2018-03-31',
+        'language'          => 'en',
         'vat_included'      => 0,
-        'esr'               => 0, //If you are using the swiss ESR set this to 1, ESR+ = 2, Red ES = 3
         'discount'          => 5, //5 % discount
         'discount_type'     => 0, //Set to 1 if you are not passing % in 'discount'
-        'positions'         => array($objPosition)
+        'positions'         => array($position)
     ));
 
-    $objInvoiceResponse = $smallinvoice->invoice_add($objInvoice);
+    $response = $smallinvoice->invoice_add($invoice);
 
+	echo "Finished" . PHP_EOL;
 } catch (\Exception $e) {
-    echo 'An error occured with number <b>' . $e->getCode() . '</b> and message <b>' . $e->getMessage() . '</b>';
+    echo 'An error occured with number <b>' . $e->getCode() . '</b> and message <b>' . $e->getMessage() . '</b>' . PHP_EOL;
 }
